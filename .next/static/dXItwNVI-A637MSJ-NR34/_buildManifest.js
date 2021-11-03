d00662693f0ddc05d77f0f19d550b734cca3ac3c
jeff.schulz@usc.edu <jeffschulz@JEFFs-MacBook-Pro.local>
2021-11-02 18:30:50 -0700

    ...

--- .next/static/dXItwNVI-A637MSJ-NR34/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef46db3751d8e999.css","static/chunks/pages/index-c15d63c6f4f9c4b0.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
